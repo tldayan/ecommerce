81a1cd86c196755640daab1905bf3fec671a0447
tldayan <110288123+tldayan@users.noreply.github.com>
2024-01-20 23:15:36 +0400

    Updated

--- src/app/components/EmptyWishlist/EmptyWishlist.tsx
+++ src/app/components/EmptyWishlist/EmptyWishlist.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import styles from "./EmptyWishlist.module.css"
-import { lato } from '../../../../libs/allFonts'
+import { lato, philosopher } from '../../../../libs/allFonts'
 
 export default function EmptyWishlist() {
   return (
     <div className={styles.emptyWishlist}>
         <h3 className={lato.className}>Your Wishlist Is Empty!</h3>
         <p>Ready to create your wishlist? Explore our amazing collection and start adding items you love!</p>
+        <button className={philosopher.className}>Start Shopping!</button>
     </div>
   )
 }
--- src/app/pages/NotFound/page.tsx
+++ src/app/pages/NotFound/page.tsx
@@ -2,8 +2,23 @@
 
 import React from 'react'
 
+import styles from "./notfound.module.css"
+import { lato, philosopher } from '../../../../libs/allFonts'
+
 export default function notFound() {
   return (
-    <div>requested product notFound</div>
+    <div className={styles.container}>
+        <div className={styles.row}>
+            <div>
+              <div className={styles.gif}>
+              </div>
+              <div className={styles.content}>
+                <h3 className={lato.className}>Page not found!</h3>
+                <p>are you sure you want to be here?</p>
+                <a href="/" className={styles.link}>Go to Home</a>
+              </div>
+            </div>
+        </div>
+      </div>
   )
 }
